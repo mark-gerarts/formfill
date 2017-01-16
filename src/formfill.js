e5738c8f15616999d4fac0d236b5c8796ab6c253
Mark Gerarts <mark.gerarts@gmail.com>
2017-01-16 22:42:20 +0100

    Able to fill some basic fields

--- src/formfill.js
+++ src/formfill.js
@@ -1,20 +1,37 @@
-FormFill = {};
+(function (faker) {
+    FormFill = {};
 
-// Main function. Fetches each form element and fills it.
-FormFill.fill = function() {
-    var self = this;
+    // Main function. Fetches each form element and fills it.
+    FormFill.fill = function() {
+        document.querySelectorAll('form').forEach(this.fillForm);
+    };
 
-    document.querySelectorAll('form').forEach(function (form) {
+    FormFill.fillForm = function (form) {
         if (typeof form.elements === 'undefined') return;
 
-        [... form.elements].forEach(self.fillElement);
-    });
-};
+        [... form.elements]
+        .filter(FormFill.shouldFillElement)
+            .forEach(FormFill.fillElement);
+    };
 
-FormFill.fillElement = function(element) {
-    console.log('filling ' + element.name);
-};
+    FormFill.shouldFillElement = function(element) {
+        // Skip hidden form elements.
+        return element.type !== 'hidden';
+    };
 
-FormFill.fill();
+    FormFill.fillElement = function(element) {
+        console.log(element.type);
+        switch(element.type) {
+            case 'select-one':
+            case 'select-multiple':
+                return;
+            case 'email':
+                element.value = faker.internet.email();
+                break;
+            default:
+                element.value = faker.random.word();
+        }
+    };
 
-console.log(faker)
+    FormFill.fill();
+})(faker);
