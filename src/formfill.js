2791c9589787286240d249cf492a64133abc822d
Mark Gerarts <mark.gerarts@gmail.com>
2017-01-16 21:52:38 +0100

    Added function to fetch elements

--- src/formfill.js
+++ src/formfill.js
@@ -1,1 +1,18 @@
-console.log('Executed from the page');
+FormFill = {};
+
+// Main function. Fetches each form element and fills it.
+FormFill.fill = function() {
+    var self = this;
+
+    document.querySelectorAll('form').forEach(function (form) {
+        if (typeof form.elements === 'undefined') return;
+
+        [... form.elements].forEach(self.fillElement);
+    });
+};
+
+FormFill.fillElement = function(element) {
+    console.log('filling ' + element.name);
+};
+
+FormFill.fill();
