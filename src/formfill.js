94165f2cd8ac38095aaa258f62b6e0218cb456b7
Mark Gerarts <mark.gerarts@gmail.com>
2017-01-17 22:14:35 +0100

    added start of a function to guess a good faker method

--- src/formfill.js
+++ src/formfill.js
@@ -48,13 +48,30 @@ function fillElement(element) {
     switch(element.type) {
         case 'select-one':
         case 'select-multiple':
+            // @todo:
+            // should use one or more of possible
+            // the possible options in stead of a
+            // faker method.
             return;
-        case 'email':
-            element.value = faker.internet.email();
-            break;
         default:
-            element.value = faker.random.word();
+            element.value = guessFakerMethod(element)();
     }
+}
+
+/**
+ * Guesses the faker method for the given form field.
+ * Returns a callable.
+ *
+ * @param element
+ * @returns {*}
+ */
+function guessFakerMethod(element) {
+    if (element.type == 'email') {
+        return faker.internet.email;
+    }
+
+    // As a last resort, return a random word.
+    return faker.random.word;
 }
 
 fill();
