925af9033695b1d8878a89ee373ebfdba4e9f517
Mark Gerarts <mark.gerarts@gmail.com>
2017-01-17 22:00:09 +0100

    Added disabled elements to the ignore list + added comments

--- src/formfill.js
+++ src/formfill.js
@@ -1,8 +1,15 @@
-// Main function. Fetches each form element and fills it.
+/**
+ * Main function. Fetches each form element and fills it.
+ */
 function fill() {
     document.querySelectorAll('form').forEach(fillForm);
 }
 
+/**
+ * Fills a single form.
+ *
+ * @param form
+ */
 function fillForm(form) {
     if (typeof form.elements === 'undefined') return;
 
@@ -11,11 +18,32 @@ function fillForm(form) {
         .forEach(fillElement);
 }
 
+/**
+ * Checks if the given element should be filled or ignored.
+ * E.g. an input with type="hidden" wil lbe ignored.
+ *
+ * @param element
+ * @returns {boolean}
+ */
 function shouldFillElement(element) {
     // Skip hidden form elements.
-    return element.type !== 'hidden';
+    if (element.type == 'hidden') {
+        return false;
+    }
+    // Skip disabled elements.
+    if (element.disabled) {
+        return false;
+    }
+
+    // Nothing is stopping us to fill this element.
+    return true;
 }
 
+/**
+ * Fills a single element.
+ *
+ * @param element
+ */
 function fillElement(element) {
     switch(element.type) {
         case 'select-one':
