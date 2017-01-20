07d7d9ac96aaa451fb842071f9fdac48b6d29456
Mark Gerarts <mark.gerarts@gmail.com>
2017-01-20 23:52:31 +0100

    CLojurized a first few functions

--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -1,3 +1,20 @@
 (ns formfill.core)
 
-(js/alert "Hello, FormFill!")
+(defn find-forms
+  "Finds every form on the page."
+  []
+  (.querySelectorAll js/document "form"))
+
+(defn fill-form
+  "Tries to fill a given form."
+  [form]
+  ())
+
+(defn should-fill-element?
+  "Checks if a given input element should be filled. For
+  example, we don't want to fill a hidden input."
+  [input-el]
+  (let [excluded-types ["hidden" "submit" "button" "image" "reset"]])
+  (not (or
+        (.-disabled input-el)
+        (contains? excluded-types (.-type input-el)))))
